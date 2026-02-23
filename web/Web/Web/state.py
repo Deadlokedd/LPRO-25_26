12ae4e9c3c7dc284b8c5bd442369ee14cf9a6457
mgabin <mikigabinrey@gmail.com>
2026-02-23 13:14:31 +0100

    Changed directoly file

--- web/Web/Web/state.py
+++ web/Web/Web/state.py
@@ -1,20 +1,22 @@
-import reflex as rx
 import os
+
+import reflex as rx
+
 
 class VideoState(rx.State):
     video_actual: str = ""
 
     @rx.var
     def biblioteca(self) -> dict[str, list[str]]:
-        base_path = "../videos"
+        base_path = "assets/videos"
         diccionario = {}
         if os.path.exists(base_path):
             for carpeta in sorted(os.listdir(base_path)):
                 ruta_carpeta = os.path.join(base_path, carpeta)
                 if os.path.isdir(ruta_carpeta):
                     videos = [
-                        f"videos/{carpeta}/{f}" 
-                        for f in os.listdir(ruta_carpeta) 
+                        f"videos/{carpeta}/{f}"
+                        for f in os.listdir(ruta_carpeta)
                         if f.endswith((".mp4", ".webm", ".mov"))
                     ]
                     if videos:
